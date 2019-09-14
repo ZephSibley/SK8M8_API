3d56c6104df35bf2ce87e4b2676efa6900dbb731
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-09-14 15:04:49 +0100

    Following old signalr sample code

---
+++ Sk8M8_API/Hubs/PrivateChatHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace PrivateChat
+{
+    public class PrivateChatHub : Hub
+    {
+        private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
+
+        public void Send(string message)
+        {
+            string name = Context.QueryString["name"];
+
+            Clients.All.SendAsync(name + ": " + message);
+        }
+
+        public void Send(string who, string message)
+        {
+            string name = Context.QueryString["name"];
+
+            foreach (var connectionId in _connections.GetConnections(who))
+            {
+                Clients.Client(connectionId).SendAsync(name + ": " + message);
+            }
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            string name = Context.QueryString["name"];
+
+            _connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            string name = Context.QueryString["name"];
+
+            _connections.Remove(name, Context.ConnectionId);
+
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
