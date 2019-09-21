932834f3379a4c1cf25005d1bfcb9cbf1a9156f6
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-09-21 16:15:56 +0100

    Getting signalR stuff down

--- Sk8M8_API/Hubs/ChatHub.cs
+++ Sk8M8_API/Hubs/ChatHub.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
-{
+{  
+    [Authorize]
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.User(user).SendAsync("ReceiveMessage", new
+            {
+                Sender = Context.User.Identity.Name,
+                Message = message
+            });
         }
     }
 }
