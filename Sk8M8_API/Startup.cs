b365266f39c92a1ea29ce41c7c6d71c2874b8c6d
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-10-01 19:51:16 +0100

    added jwt auth for websockets missed in the merge

--- Sk8M8_API/Startup.cs
+++ Sk8M8_API/Startup.cs
@@ -64,6 +64,25 @@ namespace Sk8M8_API
                     ValidateIssuer = true,
                     ValidateAudience = true
                 };
+                // Allow JWT Auth handler to read access tokens
+                // on incoming WebSocket/Server-Sent Event requests
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/hubs/chat")))
+                        {
+                            // Read the token out of the query string
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
             services.AddScoped<Services.ISessionManagementService, Services.SessionManagementService>();
