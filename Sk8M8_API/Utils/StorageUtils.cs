0a01bc807aa03a9410cd8c351c07bd4ec8c27e98
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-21 12:27:14 +0000

    on-demand virus scan function

--- Sk8M8_API/Utils/StorageUtils.cs
+++ Sk8M8_API/Utils/StorageUtils.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using nClam;
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Sk8M8_API
@@ -7,6 +10,32 @@ namespace Sk8M8_API
     // TODO: Configure Azure blob storage
     public class StorageUtils
     {
+        /// <summary>
+        /// On-demand virus scanner for temp files.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns>bool</returns>
+        public static async Task<bool> FileIsSafe(string filePath)
+        {
+            var clam = new ClamClient("localhost", 3310);
+            var scanResult = await clam.ScanFileOnServerAsync(filePath);
+
+            switch (scanResult.Result)
+            {
+                case ClamScanResults.Clean:
+                    return true;
+                case ClamScanResults.VirusDetected:
+                    Console.WriteLine("Virus Found!");
+                    Console.WriteLine("Virus name: {0}", scanResult.InfectedFiles.First().VirusName);
+                    return false;
+                case ClamScanResults.Error:
+                    Console.WriteLine("Woah an error occured! Error: {0}", scanResult.RawResult);
+                    return false;
+                default:
+                    Console.WriteLine("VIRUS SCAN DEFAULTING");
+                    return false;
+            }
+        }
         public static async Task<string> StoreImageFile(IFormFile file)
         {
             // TODO: Strip EXIF
