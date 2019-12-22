487bf46cf829f41ddeaaa290d941ad6ee5c1df50
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-22 10:11:48 +0000

    added a compose function

---
+++ Sk8M8_API/Utils/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sk8M8_API.Utils
+{
+    public static class Extensions
+    {
+        public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2)
+        {
+            return x => func1(func2(x));
+        }
+    }
+}
