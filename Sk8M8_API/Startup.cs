c69d4c4b94f225ad92e1e1e1674253abf9618a6c
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-30 14:38:10 +0000

    tidying up imports

--- Sk8M8_API/Startup.cs
+++ Sk8M8_API/Startup.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Sk8M8_API.Models;
 using Microsoft.IdentityModel.Tokens;
 using Sk8M8_API.DataClasses;
 using System.Text;
 using SignalRChat.Hubs;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
