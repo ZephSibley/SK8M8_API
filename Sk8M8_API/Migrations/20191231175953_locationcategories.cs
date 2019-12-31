30055fd4c05f1bdca6c9dd330d7d43857fd84561
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2019-12-31 18:16:40 +0000

    locationcategories migrations

---
+++ Sk8M8_API/Migrations/20191231175953_locationcategories.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using GeoAPI.Geometries;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Sk8M8_API.Migrations
+{
+    public partial class locationcategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocationCategory");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.AlterColumn<IPoint>(
+                name: "Geolocation",
+                table: "Client",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "LocationType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarkerCategory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    MapMarkerId = table.Column<long>(nullable: true),
+                    LocationTypeId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarkerCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MarkerCategory_LocationType_LocationTypeId",
+                        column: x => x.LocationTypeId,
+                        principalTable: "LocationType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MarkerCategory_MapMarker_MapMarkerId",
+                        column: x => x.MapMarkerId,
+                        principalTable: "MapMarker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkerCategory_LocationTypeId",
+                table: "MarkerCategory",
+                column: "LocationTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkerCategory_MapMarkerId",
+                table: "MarkerCategory",
+                column: "MapMarkerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MarkerCategory");
+
+            migrationBuilder.DropTable(
+                name: "LocationType");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Geolocation",
+                table: "Client",
+                nullable: true,
+                oldClrType: typeof(IPoint),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocationCategory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CategoryId = table.Column<long>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    MapMarkerId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LocationCategory_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LocationCategory_MapMarker_MapMarkerId",
+                        column: x => x.MapMarkerId,
+                        principalTable: "MapMarker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationCategory_CategoryId",
+                table: "LocationCategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationCategory_MapMarkerId",
+                table: "LocationCategory",
+                column: "MapMarkerId");
+        }
+    }
+}
