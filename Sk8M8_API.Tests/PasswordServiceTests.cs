e541382b212dbf1ece5ac4ffbb57a6d48edcb0a8
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2020-02-02 12:04:20 +0000

    Brought in xunit, first tests for password service

---
+++ Sk8M8_API.Tests/PasswordServiceTests.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+using Sk8M8_API.Services;
+
+namespace Sk8M8_API.Tests
+{
+    public class PasswordServiceTests
+    {
+
+        [Fact]
+        public void Hashing()
+        {
+            var password = Services.PasswordService.HashPassword("test string");
+            Assert.IsType<string>(password);
+        }
+        [Fact]
+        public void CheckingValid()
+        {
+            var testString = "test string two";
+
+            var password = Services.PasswordService.HashPassword(testString);
+            Assert.IsType<string>(password);
+
+            var checkSuccess = Services.PasswordService.CheckPassword(
+                testString,
+                password
+            );
+            Assert.StrictEqual<Enums.ValidatePasswordStatus>(
+                Enums.ValidatePasswordStatus.Valid,
+                checkSuccess
+            );
+        }
+        [Fact]
+        public void CheckingInvalid()
+        {
+            var password = Services.PasswordService.HashPassword("password");
+            Assert.IsType<string>(password);
+
+            var checkSuccess = Services.PasswordService.CheckPassword(
+                "non-matching password",
+                password
+            );
+            Assert.StrictEqual<Enums.ValidatePasswordStatus>(
+                Enums.ValidatePasswordStatus.Invalid,
+                checkSuccess
+            );
+        }
+    }
+}
