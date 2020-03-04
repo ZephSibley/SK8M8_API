596a092e2fd489f81142a676c204c9ee2966e797
Zeph <zeph.sibley@yahoo.com>
2020-03-04 18:39:59 +0000

    Shouldn't use hsts on an API, it's a browser instruction

--- Sk8M8_API/Startup.cs
+++ Sk8M8_API/Startup.cs
@@ -105,15 +105,9 @@ namespace Sk8M8_API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseRouting();
             app.UseAuthentication();
-            app.UseHttpsRedirection();
             app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
