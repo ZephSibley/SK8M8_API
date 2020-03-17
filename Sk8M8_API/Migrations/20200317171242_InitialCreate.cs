cd79a9d7e1761e99c0b2b35c6e75ca1da37ec67a
Zeph <zeph.sibley@yahoo.com>
2020-03-17 17:23:42 +0000

    initial migrations

---
+++ Sk8M8_API/Migrations/20200317171242_InitialCreate.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Sk8M8_API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Username = table.Column<string>(nullable: true),
+                    Geolocation = table.Column<Point>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocationType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permission",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permission", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientLogin",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    ClientId = table.Column<long>(nullable: true),
+                    IPAddress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientLogin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientLogin_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Media",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    ClientId = table.Column<long>(nullable: true),
+                    Filename = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Media", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Media_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPermission",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    ClientId = table.Column<long>(nullable: true),
+                    PermissionId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPermission", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserPermission_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserPermission_Permission_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permission",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MapMarker",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    LocationCategory = table.Column<string>(nullable: true),
+                    Point = table.Column<Point>(nullable: true),
+                    VideoId = table.Column<long>(nullable: true),
+                    CreatorId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapMarker", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MapMarker_Client_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MapMarker_Media_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Media",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MediaRating",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    MediaId = table.Column<long>(nullable: true),
+                    ClientId = table.Column<long>(nullable: true),
+                    Rating = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaRating", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MediaRating_Client_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MediaRating_Media_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "Media",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientMarker",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<long>(nullable: true),
+                    MapMarkerId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientMarker", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientMarker_MapMarker_MapMarkerId",
+                        column: x => x.MapMarkerId,
+                        principalTable: "MapMarker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClientMarker_Client_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Client",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarkerCategory",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    DateUpdated = table.Column<DateTime>(nullable: false),
+                    MapMarkerId = table.Column<long>(nullable: true),
+                    LocationTypeId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarkerCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MarkerCategory_LocationType_LocationTypeId",
+                        column: x => x.LocationTypeId,
+                        principalTable: "LocationType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MarkerCategory_MapMarker_MapMarkerId",
+                        column: x => x.MapMarkerId,
+                        principalTable: "MapMarker",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientLogin_ClientId",
+                table: "ClientLogin",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientMarker_MapMarkerId",
+                table: "ClientMarker",
+                column: "MapMarkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientMarker_UserId",
+                table: "ClientMarker",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapMarker_CreatorId",
+                table: "MapMarker",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MapMarker_VideoId",
+                table: "MapMarker",
+                column: "VideoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkerCategory_LocationTypeId",
+                table: "MarkerCategory",
+                column: "LocationTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkerCategory_MapMarkerId",
+                table: "MarkerCategory",
+                column: "MapMarkerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Media_ClientId",
+                table: "Media",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaRating_ClientId",
+                table: "MediaRating",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaRating_MediaId",
+                table: "MediaRating",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPermission_ClientId",
+                table: "UserPermission",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPermission_PermissionId",
+                table: "UserPermission",
+                column: "PermissionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientLogin");
+
+            migrationBuilder.DropTable(
+                name: "ClientMarker");
+
+            migrationBuilder.DropTable(
+                name: "MarkerCategory");
+
+            migrationBuilder.DropTable(
+                name: "MediaRating");
+
+            migrationBuilder.DropTable(
+                name: "UserPermission");
+
+            migrationBuilder.DropTable(
+                name: "LocationType");
+
+            migrationBuilder.DropTable(
+                name: "MapMarker");
+
+            migrationBuilder.DropTable(
+                name: "Permission");
+
+            migrationBuilder.DropTable(
+                name: "Media");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
