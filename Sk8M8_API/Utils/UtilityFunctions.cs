26e42a393bfa8c1e573b073741eb283619eabf5d
Zeph <zeph.sibley@yahoo.com>
2020-04-25 14:08:19 +0100

    missed from previous commit

---
+++ Sk8M8_API/Utils/UtilityFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sk8M8_API.Utils
+{
+    public static class UtilityFunctions
+    {
+        public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2)
+        {
+            return x => func1(func2(x));
+        }
+    }
+}
