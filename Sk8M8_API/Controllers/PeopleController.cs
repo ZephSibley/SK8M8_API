2408ea1bbfd397143210905fd9495d465d9c2c6e
Zeph <zeph.sibley@yahoo.com>
2020-04-25 14:46:01 +0100

    Missed from previous commit

---
+++ Sk8M8_API/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Sk8M8_API.Models;
+
+namespace Sk8M8_API.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private SkateContext Context { get; set; }
+        public PeopleController(SkateContext context)
+        {
+            Context = context;
+        }
+        public ActionResult Find(
+            double Latitude,
+            double Longitude,
+            double Radius
+        )
+        {
+            var currentLocation = StorageUtils.CreateGeoPoint(Latitude, Longitude);
+
+            var discoveredPeople = Context.Client
+                .Where(row => row.Geolocation.IsWithinDistance(currentLocation, Radius))
+                .Select(row => new { row.Avatar, row.Username, row.Status })
+                .ToList();
+
+            return Json(discoveredPeople);
+        }
+    }
+}
