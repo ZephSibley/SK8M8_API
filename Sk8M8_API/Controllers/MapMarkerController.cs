61b0cb09eff693923d62f0ab0b553e6e0825a624
Zeph <zeph.sibley@yahoo.com>
2020-04-25 14:05:45 +0100

    adjusting mapmarker proximity check to be more fine-grained

--- Sk8M8_API/Controllers/MapMarkerController.cs
+++ Sk8M8_API/Controllers/MapMarkerController.cs
@@ -44,7 +44,7 @@ namespace Sk8M8_API.Controllers
 
             var markerPoint = StorageUtils.CreateGeoPoint(marker.Latitude, marker.Longitude);
             var proximityCheck = Context.MapMarker
-                .Where(row => row.Point.IsWithinDistance(markerPoint, 0.2))
+                .Where(row => row.Point.IsWithinDistance(markerPoint, 0.000005))
                 .Any();
             if (proximityCheck)
             {
