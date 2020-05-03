c4756a94d56545d952f045c912b5096d63406b76
Zeph <zeph.sibley@yahoo.com>
2020-05-03 19:41:51 +0100

    Added min length for passwords

--- Sk8M8_API/DataClasses/UserCreationRequest.cs
+++ Sk8M8_API/DataClasses/UserCreationRequest.cs
@@ -9,7 +9,7 @@ namespace Sk8M8_API.DataClasses
         public string Username { get; set; }
 
         [Required]
-        [StringLength(101, ErrorMessage = "Password is too long")]
+        [StringLength(101, ErrorMessage = "Password is too long", MinimumLength = 6)]
         public string Password { get; set; }
 
         [Required]
