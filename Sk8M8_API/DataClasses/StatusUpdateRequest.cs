756816fbfd5bc07408afd32f73db206ae4ceab5a
Zeph <zeph.sibley@yahoo.com>
2020-05-04 19:40:51 +0100

    reduced max length of statuses

--- Sk8M8_API/DataClasses/StatusUpdateRequest.cs
+++ Sk8M8_API/DataClasses/StatusUpdateRequest.cs
@@ -5,7 +5,7 @@ namespace Sk8M8_API.DataClasses
     public class StatusUpdateRequest
     {
         [Required]
-        [StringLength(200, ErrorMessage = "Status is too long")]
+        [StringLength(150, ErrorMessage = "Status is too long")]
         public string Status { get; set; }
     }
 }
